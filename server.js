7b42eeae83d5ffc5676cc9fb43761d999d931e38
osquitoOfTheNorth <oscar.menjivar@utoronto.ca>
2017-09-04 23:28:59 -0400

    Fixed node js code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = 8000;
+const port = process.env.PORT || 5000;
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser').MongoClient
