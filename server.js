f5a91e17a4ad38e5b2468778fdc2109a7a5ba6ac
osquitoOfTheNorth <oscar.menjivar@utoronto.ca>
2017-09-04 23:05:19 -0400

    Added dependencies

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const port = 8000;
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('bodyParser').MongoClient
+
 
 app.listen(port, () => {
 	console.log("Live on the internet!!");
