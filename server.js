5ae03ab55a1241d7532bdf0b00936bc0a89b5313
osquitoOfTheNorth <oscar.menjivar@utoronto.ca>
2017-09-04 23:09:45 -0400

    Added Server.js File

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const port = 8000;
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
-const bodyParser = require('bodyParser').MongoClient
+const bodyParser = require('body-parser').MongoClient
 
-
+const app = express();
 app.listen(port, () => {
 	console.log("Live on the internet!!");
 })
